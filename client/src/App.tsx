e9da417c899d9fff4f966415cc3f25136f068547
themd <themd@mail.ru>
2024-02-29 19:39:59 +0500

    feat(client): update routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,12 @@ import AdminPage from './layout/AdminPage'
 import Home from './layout/Home'
 // import Login from './layout/Login'
 import NotFound from './layout/NotFound'
-import Register from './layout/Register'
+// import Register from './layout/Register'
 
 const App = () => (
     <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />}/>
+        {/* <Route path="/register" element={<Register />}/> */}
         {/* <Route path="/login" element={<Login />}/> */}
         <Route path="/admin/*" element={<AdminPage/>}/>
         <Route path="*" element={<NotFound />} />
--- client/src/layout/AdminPage.tsx
+++ client/src/layout/AdminPage.tsx
@@ -1,12 +1,14 @@
 import { authProvider } from '../services/authProvider'
 import { Dashboard } from '../components/Dashboard'
 import { Layout, Login } from './index'
-import { Admin, Resource } from 'react-admin'
+import { Admin, Resource, CustomRoutes } from 'react-admin'
+import { Route } from 'react-router-dom'
 import { SiteCreate, SiteEdit, SiteList } from '../views/sites'
 import SiteIcon from '@mui/icons-material/Book'
 import { NotificationCreate, NotificationEdit, NotificationList } from '../views/notifications'
 import jsonServerProvider from 'ra-data-json-server'
 import { httpClient } from '../services/httpClient'
+import Register from '../layout/Register'
 
 const dataProvider = jsonServerProvider(import.meta.env.APP_API_URL, httpClient)
 
@@ -34,6 +36,9 @@ const AdminPage = () => (
             create={NotificationCreate}
             icon={SiteIcon}
         />
+        <CustomRoutes>
+            <Route path="/register" element={<Register/>}/>
+        </CustomRoutes>
     </Admin>
 )
 
