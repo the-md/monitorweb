0369407b2f839bfd3c68005e50d8a47c25745530
themd <themd@mail.ru>
2024-03-13 18:00:40 +0500

    feat(server): edit notification model

--- server/models/notification-model.js
+++ server/models/notification-model.js
@@ -4,6 +4,8 @@ const {ObjectId} = require("mongodb");
 const NotificationSchema = new Schema({
     type: {type: String, default: 'email'},
     address: {type: String},
+    tgName: {type: String},
+    tgCode: {type: String},
     userId: {type: Schema.Types.ObjectId, ref: 'User'}
 })
 
